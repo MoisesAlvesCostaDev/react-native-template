2c4d7938e2ff5a7b472e0d1a0654f7d50d1dff52
Moises Alves Costa <moi.ac@hotmail.com>
2021-12-16 21:22:16 -0300

    feat: configurações iniciais

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = function App():JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Ola mundo</Text>
+    </SafeAreaView>
+
+  );
+};
+
+export default App;
