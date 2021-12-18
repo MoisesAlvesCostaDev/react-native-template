38d37c34f489349de5e0be18230ee96e764c21cd
Moises Alves Costa <moi.ac@hotmail.com>
2021-12-18 11:51:51 -0300

    feat: configuração importações caminhos absolutos

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        cwd: "babelrc",
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+          ".android.js",
+          ".android.tsx",
+          ".ios.js",
+          ".ios.tsx",
+        ],
+        root: ["src"],
+      },
+    ],
+    "jest-hoist",
+  ],
 };
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+export const valor = 10;
 
 AppRegistry.registerComponent(appName, () => App);
