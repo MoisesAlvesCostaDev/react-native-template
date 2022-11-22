5dd4cb1bda2f7b6ef278e2bdf727d7f8611160c0
Moises Alves Costa <moi.ac@hotmail.comm>
2022-11-21 21:23:34 -0300

    test: Teste no component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,36 +1,10 @@
 import React from "react";
-import {StyledTouchableOpacity,StyledText} from './style';
+import { StyledTouchableOpacity, StyledText } from "./style";
 
-
-
-export default function Button({title="Button title",...props}) {
+export default function Button({ title = "Button title", ...props }) {
   return (
-    <StyledTouchableOpacity {...props}>
+    <StyledTouchableOpacity testID="buttonStyled" {...props}>
       <StyledText>{title}</StyledText>
     </StyledTouchableOpacity>
   );
 }
-
-// {
-//   name,
-//   label,
-//   onChangeText,
-//   ...rest
-// }: InputProps
-
-
-// interface ButtonProps extends TouchableOpacityProps {
-//   title: string
-// }
-// interface InputReference extends TextInput {
-//   value: string
-// }
-
-
-// interface InputProps extends TextInputProps {
-//   name: string
-//   label: string
-// }
-// interface InputReference extends TextInput {
-//   value: string
-// }
